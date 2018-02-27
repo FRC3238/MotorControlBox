94f594209f7d1aa2cf5bfdcbe17437c9b96ee85b
wejdev <wejdev@gmail.com>
2018-02-27 15:16:36 -0800

    Add servo controlling code

--- MotorControlBox.ino
+++ MotorControlBox.ino
@@ -1,129 +1,98 @@
-4df
-#include <LiquidCrystal.h>; 
 #include <Servo.h>;
+#include <String.h>
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
+// Move any other LCD libraries to another folder or delete them
+#include <LiquidCrystal_I2C.h>
 
-Servo servo;
-LiquidCrystal lcd(12, 11, 6, 5, 3, 2);
-
-int sensorValue;
-int percent;
-int servoValue;
-int state;
-int flip;//flip is either -1, 0 or 1. this will be a multiplier for the end output to the jaguar, to "flip" the output to negative, posisitve, or nothing.
-int percentShow;
-
-// the setup routine runs once when you press reset:
-void setup() 
+enum directions
 {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  servo.attach(9);
-  state = 0;
-  flip = 0;
-  pinMode(7, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);//pin 19 on ATMega chip without Arduino Board
-  pinMode(10, INPUT_PULLUP);//pin 16 on ATMega chip without Arduino Board
-  lcd.setCursor(0,0);
-  lcd.print("MotoCont 2.2");
-  delay(1500);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("By Nick Sorensen");
-  lcd.setCursor(0,1);
-  lcd.print("And Cody Frost");
-  delay(2000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Also...");
-  lcd.setCursor(0,1);
-  lcd.print("A. Sorb'jorben");
-  delay(3000);
+  forward,
+  neutral,
+  reverse
+};
+
+// Set the pins on the I2C chip used for LCD connections:
+//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); // Set the LCD I2C address
+Servo servo;
+
+const int PULLED_DOWN = 0;
+
+const int percentageSettingPin = A0;
+const int enableSwitchPin = 5;
+const int forwardSwitchPin = 6;
+const int reverseSwitchPin = 7;
+const int servoPin = 9;
+
+int sensorValue = 0;
+int percent = 0;
+
+directions direction = neutral;
+bool enabled = false;
+
+void setup()
+{
+  pinMode(enableSwitchPin, INPUT_PULLUP);
+  pinMode(forwardSwitchPin, INPUT_PULLUP);
+  pinMode(reverseSwitchPin, INPUT_PULLUP);
+  pinMode(percentageSettingPin, INPUT);
+
+  servo.attach(servoPin);
+
+  lcd.begin(16, 2); // initialize the lcd for 16 chars 2 lines, turn on backlight
   lcd.clear();
 }
 
-// the loop routine runs over and over again forever:
-void loop() 
+void loop()
 {
-  sensorValue = analogRead(A5);
-  percent = map(sensorValue, 0, 1023, -100, 100);
-  servoValue = map(sensorValue, 0, 1023, 0, 179);
-  
-  if(flip == 0)
+  sensorValue = analogRead(percentageSettingPin);
+  percent = map(sensorValue, 3, 1020, 0, 100);
+  char percentageString[4];
+  itoa(percent, percentageString, 10);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (digitalRead(forwardSwitchPin) == PULLED_DOWN)
   {
-    percentShow = percent;
-    percent = percent*flip;
+    direction = forward;
+    lcd.write("Forward ");
+  }
+  else if (digitalRead(reverseSwitchPin) == PULLED_DOWN)
+  {
+    direction = reverse;
+    lcd.write("Reverse ");
   }
   else
   {
-    percent = percent*flip;
-    percentShow = percent;
+    direction = neutral;
+    lcd.write("Neutral ");
   }
-  
-  switch(flip)
-  {  
-    case 0: 
-    servoValue = 90;
-    break;
-    
-    case -1:
-    servoValue = 180+(servoValue*-1);
-    break;
-    
-    default:
-    break;
-  }
-  
-  
-  switch (state)
+  lcd.write(percentageString);
+  lcd.write("%");
+
+  enabled = digitalRead(enableSwitchPin) == PULLED_DOWN;
+  lcd.setCursor(0, 1);
+  lcd.write(enabled ? "Enabled         " : "Disabled        ");
+
+  int servoSetting = 90; // Default for neutral, enabled or not.
+  if (enabled)
   {
-     case 0: //Disabled state
-     lcd.setCursor(0,1);
-     lcd.print("State: Disabled!");
-     lcd.setCursor(0,0);
-     lcd.print("Percent: ");
-     lcd.print(percentShow);
-     lcd.print("      ");
-     servo.write(90);
-     break;
-     
-     
-     case 1:
-     lcd.setCursor(0,1);
-     lcd.print("State: Enabled! ");
-     lcd.setCursor(0,0);
-     lcd.print("Percent: ");
-     lcd.print(percentShow);
-     lcd.print("      ");
-     servo.write(servoValue);
-     break;
-     
-     default:
-     lcd.clear();
-     lcd.setCursor(0,0);
-     lcd.print("Not Sure?");
-     servo.write(90);
-     break;
-   
+    if (direction == forward)
+    {
+      servoSetting = map(percent, 0, 100, 90, 180);
+    }
+    else if (direction == reverse)
+    {
+      servoSetting = map(percent, 0, 100, 90, 0);
+    }
   }
-  
-  flip = 0;
-  if(digitalRead(13) == 0)
-  {
-    flip = 1;
-  }
-  if(digitalRead(10) == 0)
-  {
-    flip = -1;
-  }
-  
-  if(digitalRead(7) == 1)
-  {
-    state = 0;
-  }
-  if(digitalRead(7) == 0)
-  {
-    state = 1;
-  }
-  delay(10);        // delay in between reads for stability
+  servo.write(servoSetting);
+
+  delay(200);
 }
+
+bool isPulledDown(int reading)
+{
+  return reading == PULLED_DOWN;
+}
+
